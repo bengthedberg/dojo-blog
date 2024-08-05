c6938acc70c0766aecf095547ce3805efa6c6ded
bengthedberg <bengt.hedberg@gmail.com>
2024-08-05 11:58:21 +1000

    delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
   const {data: blog, isLoading, error}  = useFetch('http://localhost:8000/blogs/' + id);
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE',
+      })
+     .then(() => {
+        // Redirect to the home page after deleting a blog
+        navigate('/');
+      });
+  };
 
   return (
     <div className='blog-details'>
@@ -15,6 +26,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
 
