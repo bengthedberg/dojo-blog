3e2b211fbe98eaf63f2de02559ecbb7848a0c418
bengthedberg <bengt.hedberg@gmail.com>
2024-08-05 11:51:04 +1000

    go back to home page after creating a blog

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevent the page to be refreshed when form is submitted
@@ -26,8 +28,8 @@ const Create = () => {
       body: JSON.stringify(blog)
     })
     .then(() => {
-      console.log('Blog added successfully');
       setIsPending(false); // Reset the form state
+      navigate('/'); // Redirect to the home page after creating a new blog
     })
   };  
 
