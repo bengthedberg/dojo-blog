528efa267eca70d0d2f6a3700dc2d962cd0badc4
bengthedberg <bengt.hedberg@gmail.com>
2024-08-05 11:16:38 +1000

    add blog details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Create from './Create';
 import {BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={ <Home /> } />
+            <Route path="/blogs/:id" element={ <BlogDetails /> } />
             <Route path="/create" element={ <Create /> } />
           </Routes>
         </div>
